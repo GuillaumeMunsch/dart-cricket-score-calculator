07996725289de50d7c8b4cc0fcca657d3d064fce
Guillaume Munsch <guigui.munsch@gmail.com>
2024-04-05 01:54:18 +0200

    feat; History

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,42 @@ import { Button, Flex, TextField } from "@radix-ui/themes";
 import Cell from "./components/Cell";
 import { Player, ScoreInput, scoreInputArray } from "./game/Player";
 
-// const columnStyle: React.CSSProperties = {
-//     width: "150px",
-// }
+function useHistory<T>(defaultValue: T) {
+  const [elems, setElems] = useState<T[]>(defaultValue ? [defaultValue] : []);
+
+  const push = (value: T) => setElems([value, ...elems]);
+
+  const pop = () => {
+    const [, ...remainingElems] = elems;
+    setElems(remainingElems);
+  };
+  const [selectedElem] = elems;
+
+  return { push, pop, selectedElem };
+}
 
 function App() {
-  const [players, setPlayers] = useState<Player[]>([]);
   const [playerNameInput, setPlayerNameInput] = useState("");
+  const { pop, push, selectedElem: players } = useHistory<Player[]>([]);
 
   return (
     <Flex align="center" direction="column">
-      <TextField.Root
-        value={playerNameInput}
-        placeholder="Name..."
-        onChange={(event) => setPlayerNameInput(event.currentTarget.value)}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            setPlayers(addPlayer(players)(playerNameInput));
-            setPlayerNameInput("");
-          }
-        }}
-        style={{ width: 150 }}
-        mb="4"
-      />
+      <Flex direction="row" gapX="2">
+        <TextField.Root
+          value={playerNameInput}
+          placeholder="Name..."
+          onChange={(event) => setPlayerNameInput(event.currentTarget.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              push(addPlayer(players)(playerNameInput));
+              setPlayerNameInput("");
+            }
+          }}
+          style={{ width: 150 }}
+          mb="4"
+        />
+        {players.length > 0 && <Button onClick={pop}>Cancel</Button>}
+      </Flex>
       <Flex>
         <div>
           <Cell>Name</Cell>
@@ -47,7 +60,7 @@ function App() {
               <Button
                 ml="2"
                 color="red"
-                onClick={() => setPlayers(removePlayer(players)(name))}
+                onClick={() => push(removePlayer(players)(name))}
               >
                 -
               </Button>
@@ -59,9 +72,7 @@ function App() {
                   mr="2"
                   color="red"
                   onClick={() =>
-                    setPlayers(
-                      computeNextGameState(players)(name, scoreInput, false)
-                    )
+                    push(computeNextGameState(players)(name, scoreInput, false))
                   }
                 >
                   -
@@ -71,7 +82,7 @@ function App() {
                   ml="2"
                   color="green"
                   onClick={() =>
-                    setPlayers(computeNextGameState(players)(name, scoreInput))
+                    push(computeNextGameState(players)(name, scoreInput))
                   }
                 >
                   +
